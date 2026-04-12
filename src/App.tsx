9bf76dfff4a7c637cefad1c7fee80f827a0e8ffb
keewonf <lukasfmm@gmail.com>
2026-04-12 17:36:31 -0300

    feat: add Tip component and integrate it into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Header } from "./components/Header";
 import styles from "./app.module.css";
 
+import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+
 function App() {
-  function handleRestartGame(){
-    alert("Reiniciar o jogo!")
+  function handleRestartGame() {
+    alert("Reiniciar o jogo!");
   }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame}/>
-        <h1>Hello World!</h1>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+        
+        <Tip tip="Biblioteca para criar interfaces Web com Javascript." />
       </main>
     </div>
   );
