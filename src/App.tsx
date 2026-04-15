61323497267af54fd8b696949df2c0e722a3ae85
keewonf <lukasfmm@gmail.com>
2026-04-15 12:57:55 -0300

    feat: add restart confirmation dialog

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,13 @@ function App() {
   const ATTEMPTS_MARGIN = 2;
 
   function handleRestartGame() {
-    alert("Reiniciar o jogo!");
+    const isConfirmed = window.confirm(
+      "Você tem certeza que deseja reiniciar?",
+    );
+
+    if (isConfirmed) {
+      startGame(WORDS);
+    }
   }
 
   function startGame(words: Challenge[]) {
